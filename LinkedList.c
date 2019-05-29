e26913aef44a5693d8d953c4d8a53e65bdbfc3ce
Dihan Chen <dchen8@id415m07.cs.unb.ca>
2019-05-29 18:42:32 -0300

    PartnerA

--- LinkedList.c
+++ LinkedList.c
@@ -1,101 +1,119 @@
-/*************************************
-*
-* Sentence reversal program using Linked List based Stack
-*
-**************************************/
-#include<stdio.h>
-#include<stdlib.h>
-#include<assert.h>
-#include<string.h>
+/********************
+ *
+ * Sentence reversal program using Linked List based Stack
+ *
+ ********************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+#include <stdbool.h>
 
 #define BUFFER_SIZE 256
 
-//create the struct
+// create the struct
 struct Node_t_t
 {
-  char ∗value ;
-  struct Node_t_t ∗next;
-}
+    char *value;
+    struct Node_t_t *next;
+};
 
-//create the short form
+// create the shortform
 typedef struct Node_t_t Node_t;
 
-//function templates
-Node_t ∗newNode (const char ∗ value, Node_t ∗next) ;
-Node_t ∗ delete Node (Node_t ∗ current, char ∗∗ value) ;
-bool pop (Node_t ∗∗ head, char ∗∗ value) ;
-bool push (Node_t ∗∗ head, const char ∗ value);
+// function templates
+Node_t *newNode(const char *value, Node_t *next);
+Node_t *deleteNode(Node_t *current, char **value);
+bool pop(Node_t **head, char **value);
+bool push(Node_t **head, const char *value);
 
-/*use void as parameters to silence the gcc warnings */
-int main (void)
+/* use void as parameters to silence the gcc warnings */
+int main(void)
 {
-	//initialize our things
-	Node_t ∗Stack = NULL;
-	char buffer [BUFFER_SIZE] = { 0 } ;
+    // initialize our things
+    Node_t *Stack = NULL;
+    char buffer[BUFFER_SIZE] = { 0 };
 
-	//fill our stack from the user input
-	while(1 == scanf("%s ", buffer))
-	{
-		push(&Stack, buffer) ;
-	}
+    // fill our stack from the user input
+    while(1 == scanf("%s", buffer))
+    {
+        push(&Stack, buffer);
+    }
 
-	//write out the sentence in reverse order
-	while(Stack != NULL)
-	{
-		char ∗str= pop(&Stack);
-		if (str)
-		{
-			printf("%s ", str) ;
-			free(str);
-		}
-	}
+    // write out the sentence in reverse order
+    while(Stack != NULL)
+    {
+        char *str = NULL;
+        bool popped = pop(&Stack, &str);
+        if( popped && NULL != str )
+        {
+            printf("%s ", str);
+            free(str);
+        }
+    }
 
-	return EXIT_SUCCESS ;
+    return EXIT_SUCCESS;
 }
 
-/**
-∗ TODO: Finish implementing these functions
-∗/
-
-/**
-∗ return a NULL pointer if it failed to allocate memory
-∗ makes a copy of the string `value` on the heap(see strdup())
-∗ and stores the pointer in the newly created Node .
-∗ sets the next node to be the parameter `next`
+/**************
+* TODO: Finish implementing these functions
 */
-Node_t ∗newNode (const char ∗value, Node_t ∗next)
+
+/**
+ * return a NULL pointer if it failed to allocate memory
+ * makes a copy of the string `value` on the heap (see strdup())
+ * and stores the pointer in the newly created Node.
+ * sets the next node to be the parameter `next`
+ */
+Node_t *newNode(const char *value, Node_t *next)
 {
-	return NULL;
+  Node_t *node = malloc(sizeof(Node_t));
+  if(node == NULL)
+  {
+    return NULL;
+  }
+  char *copy = strdup(value);
+  node->value = copy;
+  node->next = next;
+  return node;
 }
 
 /**
- ∗puts the current node string pointer at the `value` parameter
- ∗free's the `current` Node.
- ∗and return the next node.
-*/
-Node_t ∗deleteNode(Node_t ∗current, char ∗∗value)
+ * puts the current node string pointer at the `value` parameter
+ * free's the ```current` Node.
+ * and return the next node.
+ */
+Node_t *deleteNode(Node_t *current, char **value)
 {
-	return NULL;
+  Node_t *newnxt = current->next;
+  *value = current->value;
+  free(current);
+  return newnxt;
 }
 
 /**
-∗ Pop the string from the top of the stack using delete Node
-∗ update the top of the stack
-∗ puts the popped string onto `value`
-∗ return true on success 
-*/
-bool pop(Node_t ∗∗ Stack, char ∗∗ value)   
+ * Pop the string from the top of the stack using deleteNode
+ * update the top of the stack
+ * puts the popped string onto `value`
+ * return true on success
+ */
+bool pop(Node_t **Stack, char **value)
 {
-	return false;
+  Node_t *top = *Stack;
+  Node_t *newnxt = deleteNode(top,value);
+  *Stack = newnxt;
+  return true;
 }
 
 /**
-∗ Push the string to the top of the stack using newNode
-∗ update the top of the stack
-∗ return true if everything is successfull
-*/
-bool push (Node_t ∗∗ Stack, const char ∗value)
+ * Push the string to the top of the stack using newNode
+ * update the top of the stack
+ * return true if everything is successfull
+ */
+bool push(Node_t **Stack, const char *value)
 {
-	return false;
+  Node_t *top = *Stack;
+  Node_t *new = newNode(value,top);
+  *Stack = new;
+  return true;
 }
-
--- makefile
+++ makefile
@@ -1,4 +1,3 @@
 # TODO: add targets to compile LinkedList.c
-# TODO: add a target for each . input file in the Data directory to run the c ompiled LinkedList
-program with the tests in ./Data
+# TODO: add a target for each . input file in the Data directory to run the c ompiled LinkedList program with the tests in ./Data
 # TODO: add a target that can run all the tests above.
